e0fe4b3fab962b0c5eed34545a51b98eda96a63b
GuyESmith <guy.e.smith@student.uts.edu.au>
2026-06-03 01:05:02 +1000

    stuff not important

--- ur3e_rl_ws/install/local_setup.sh
+++ ur3e_rl_ws/install/local_setup.sh
@@ -6,7 +6,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_prefix_sh_COLCON_CURRENT_PREFIX="/home/ollie/git/RS2/main/HoloAssist/ur3e_rl_ws/install"
+_colcon_prefix_sh_COLCON_CURRENT_PREFIX="/home/guy/git/HoloAssist-AI/ur3e_rl_ws/install"
 if [ -z "$COLCON_CURRENT_PREFIX" ]; then
   if [ ! -d "$_colcon_prefix_sh_COLCON_CURRENT_PREFIX" ]; then
     echo "The build time path \"$_colcon_prefix_sh_COLCON_CURRENT_PREFIX\" doesn't exist. Either source a script for a different shell or set the environment variable \"COLCON_CURRENT_PREFIX\" explicitly." 1>&2
